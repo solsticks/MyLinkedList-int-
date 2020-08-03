e0b50a0c17c18bb480124ec5984670ebb43ab9cf
solaadekan@gmail.com <solaadekan@gmail.com>
2020-08-03 23:17:09 +0100

    I have implemented a linked list with the following methods; addToStart, addtoEnd, addSorted

---
+++ myList/myList/MyLinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myList
+{
+    class myLinkedList
+    {
+        public Node headNode;
+
+
+        public myLinkedList()
+        {
+            headNode = null;
+
+        }
+
+        public void addEnd(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                headNode.addToEnd(data);
+            }
+        }
+
+        public void addStart(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.Next = headNode;
+                headNode = temp;
+
+            }
+        }
+
+        public void Print()
+        {
+            if (headNode != null)
+            {
+                headNode.Print();
+            }
+        }
+
+
+    }
+}
---
+++ myList/myList/Node.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myList
+{
+    class Node
+    {
+        public int data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+
+        public void addToEnd(int data)
+        {
+            if (Next == null)
+            {
+                Next = new Node(data);
+            }
+            else
+            {
+                Next.addToEnd(data);
+            }
+        }
+
+        public void addSorted(int data)
+        {
+            if (Next == null)
+            {
+                Next = new Node(data);
+            }
+            else if (data < Next.data)
+            {
+                Node temp = new Node(data);
+                temp.Next = this.Next;
+                this.Next = temp;
+            }
+            else
+            {
+                Next.addSorted(data);
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write($"{data}->");
+            if (Next != null)
+            {
+                Next.Print();
+            }
+        }
+    }
+}
